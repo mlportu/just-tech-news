0a9d85c62d2c00d27816f21a5d6c9707b5276fd4
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-02 17:17:17 -0500

    comments feature added and integrated

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User.js");
 const Post = require("./Post.js");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -27,16 +28,32 @@ Vote.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-User.hasMany(Vote, {
-    foreignKey: 'user_id'
-});
-
 Vote.belongsTo(Post, {
     foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
 });
 
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
